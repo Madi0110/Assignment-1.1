2801b98a174583bdad550959023ad9c0531753b5
Madiyar <seisembaevm@mail.ru>
2020-09-09 12:49:43 +0600

    Assignment 1.1

---
+++ src/app/Component2/component2.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-component2',
+  templateUrl: './component2.component.html',
+  styleUrls: ['./component.component.css']
+})
+export class Component2Component {
+  title = 'Component2';
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {Component2Component} from './Component2/component2.component';
+import {Component3Component} from './Component3/component3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    Component2Component,
+    Component3Component
   ],
   imports: [
     BrowserModule,
